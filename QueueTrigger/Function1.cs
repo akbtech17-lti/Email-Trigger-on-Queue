9be685b9071d700ac152a06d6b8d9019c6f61e18
anshul-lti <AnshulKumar.Bansal@lntinfotech.com>
2022-10-21 11:54:22 +0530

    extracted email

--- QueueTrigger/Function1.cs
+++ QueueTrigger/Function1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -7,10 +8,23 @@ namespace QueueTrigger
 {
     public class Function1
     {
-        [FunctionName("Function1")]
-        public void Run([QueueTrigger("transactions", Connection = "StorageAccountConn")]string myQueueItem, ILogger log)
+		public static string getBetween(string strSource, string strStart, string strEnd)
+		{
+			if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+			{
+				int Start, End;
+				Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+				End = strSource.IndexOf(strEnd, Start);
+				return strSource.Substring(Start, End - Start);
+			}
+
+			return "";
+		}
+		[FunctionName("Function1")]
+        public void Run([QueueTrigger("transactions", Connection = "StorageAccountConn")]string transactionDetails, ILogger log)
         {
-            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+			string email = getBetween(transactionDetails, "Email : ", "\n");
+            log.LogInformation($"C# Queue trigger function processed: {email} + {transactionDetails}");
         }
     }
 }
